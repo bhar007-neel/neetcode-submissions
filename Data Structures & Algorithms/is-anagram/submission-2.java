e5f61116a1a622050bc9c84482e111fe42e98478
Neelmani Bhardwaj <141062451+bhar007-neel@users.noreply.github.com>
2026-08-23 10:47:05 -0400

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.java
@@ -0,0 +1,36 @@
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        HashMap<Character, Integer> map1 = new HashMap<>();
+        HashMap<Character, Integer> map2 = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+
+            char c = s.charAt(i);
+
+            if (map1.containsKey(c)) {
+                map1.put(c, map1.get(c) + 1);
+            } else {
+                map1.put(c, 1);
+            }
+        }
+
+        for (int i = 0; i < t.length(); i++) {
+
+            char c = t.charAt(i);
+
+            if (map2.containsKey(c)) {
+                map2.put(c, map2.get(c) + 1);
+            } else {
+                map2.put(c, 1);
+            }
+        }
+
+        return map1.equals(map2);
+    }
+}
